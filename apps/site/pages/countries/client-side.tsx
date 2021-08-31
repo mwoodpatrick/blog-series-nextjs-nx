b318bc7c860cb49742e2a059c15c3ea6506cd1fb
mwoodpatrick <mwoodpatrick@gmail.com>
2021-08-30 17:47:31 -0700

    primitive graphql server & client

---
+++ apps/site/pages/countries/client-side.tsx
@@ -0,0 +1,14 @@
+import './client-side.module.css';
+
+/* eslint-disable-next-line */
+export interface ClientSideProps {}
+
+export function ClientSide(props: ClientSideProps) {
+  return (
+    <div>
+      <h1>Welcome to ClientSide!</h1>
+    </div>
+  );
+}
+
+export default ClientSide;
---
+++ apps/site/pages/countries/server-side.tsx
@@ -0,0 +1,14 @@
+import './server-side.module.css';
+
+/* eslint-disable-next-line */
+export interface ServerSideProps {}
+
+export function ServerSide(props: ServerSideProps) {
+  return (
+    <div>
+      <h1>Welcome to ServerSide!</h1>
+    </div>
+  );
+}
+
+export default ServerSide;
