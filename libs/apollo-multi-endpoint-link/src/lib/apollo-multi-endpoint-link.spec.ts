d6329558558f9e6b4ade1270c48dd831ffe3bb54
mwoodpatrick <mwoodpatrick@gmail.com>
2021-09-08 07:58:34 -0700

    Add test case for MultiAPILink remove one unused test

--- libs/apollo-multi-endpoint-link/src/lib/apollo-multi-endpoint-link.spec.ts
+++
@@ -1,7 +0,0 @@
-import { apolloMultiEndpointLink } from './apollo-multi-endpoint-link';
-
-describe('apolloMultiEndpointLink', () => {
-  it('should work', () => {
-    expect(apolloMultiEndpointLink()).toEqual('apollo-multi-endpoint-link');
-  });
-});
